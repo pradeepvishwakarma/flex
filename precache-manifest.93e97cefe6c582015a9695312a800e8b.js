d5f81574c9a4ad7d8d9ec1de5a53b794245503e1
Pradeep <vpradeep0801@gmail.com>
2019-08-18 18:04:10 +0530

    initial build push

---
+++ precache-manifest.93e97cefe6c582015a9695312a800e8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a41b1ecb579a63f7f97fe6ade90a5efb",
+    "url": "/flex/index.html"
+  },
+  {
+    "revision": "016ba7d8a0b2bcb7c49c",
+    "url": "/flex/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "31fefca5c6291a72dbd6",
+    "url": "/flex/static/js/2.ae086021.chunk.js"
+  },
+  {
+    "revision": "016ba7d8a0b2bcb7c49c",
+    "url": "/flex/static/js/main.ff146300.chunk.js"
+  },
+  {
+    "revision": "a68bdefc8f47320b005f",
+    "url": "/flex/static/js/runtime~main.dadf2f12.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/flex/static/media/logo.5d5d9eef.svg"
+  }
+]);
